409f3271d7373b2346aed4e095a309bce74eb9a5
LiaPuigmi <liapuigmi@gmail.com>
2023-12-05 13:41:26 +0100

    cross

--- src/main/java/com/example/controllers/DirectorController.java
+++ src/main/java/com/example/controllers/DirectorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +19,7 @@ import com.example.services.DirectorService;
 
 @RestController
 @RequestMapping("/directores")
+@CrossOrigin(origins="*")
 public class DirectorController {
 	@Autowired
 	DirectorService directorService;
