b55723af5df6ff2a8177e8857f2952fed640085c
SergioSerranoo <sergioserranocesar@gmail.comm>
2023-12-07 11:57:07 +0100

    add check user

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.example.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +32,7 @@ public class UserController {
 		return userService.findAllUsers();
 	}
 
-	@GetMapping(value = "/")
+	@PostMapping(value = "/login")
 	public AddUserResult getUsuarios(@RequestParam(required = true) String id, @RequestParam(required = true) String password) {
 		User.info("Request a http://localhost:PORT/user/id(GET)");
 		
