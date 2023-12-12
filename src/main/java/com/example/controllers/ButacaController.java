0ae3597c57dccae5d7434f7895a9622a466c9f6c
LiaPuigmi <liapuigmi@gmail.com>
2023-12-12 13:49:21 +0100

    butacas liadilla

---
+++ src/main/java/com/example/controllers/ButacaController.java
@@ -0,0 +1,63 @@
+package com.example.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entities.AddUserResult;
+import com.example.entities.Butaca;
+import com.example.services.ButacaService;
+
+@RestController
+@RequestMapping("/butacas")
+@CrossOrigin(origins="*")
+public class ButacaController {
+	@Autowired
+	ButacaService butacaService;
+
+	@GetMapping
+	public List<Butaca> getAllButacas(){
+		return butacaService.findAllButacasTotal();
+	}
+	
+	@GetMapping (value = "/{idcine}/{idsala}")
+	public List<Butaca> getButacas(@PathVariable int idcine,@PathVariable int idsala) {
+		Butaca.info("Request a http://localhost:PORT/butacas(GET)");
+		return butacaService.findAllButacas(idcine, idsala);
+	}
+
+	@GetMapping(value = "/{idcine}/{idsala}/{idfila}/{idcolumna}")
+	public AddUserResult getButaca(@PathVariable int idcine,@PathVariable int idsala,@PathVariable int idfila,@PathVariable int idcolumna) {
+		Butaca.info("Request a http://localhost:PORT/butacas/id(GET)");
+		return butacaService.findButacaById(getButacas(idcine, idsala), idfila, idcolumna);
+	}
+
+	@PutMapping
+	public Butaca addButaca(@RequestBody Butaca butaca) {
+		Butaca.info("Request a http://localhost:PORT/butacas/add(PUT)");
+		return butacaService.addButaca(butaca);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public void deleteButaca(@PathVariable int id) {
+		Butaca.info("Request a http://localhost:PORT/butacas/delete/id(DELETE)");
+
+		butacaService.deleteButaca(id);
+	}
+
+	@PatchMapping("/update")
+	public Butaca updateButaca(@RequestBody Butaca butaca) {
+		Butaca.info("Request a http://localhost:PORT/butacas/add(PUT)");
+		return butacaService.updateButaca(butaca);
+	}
+}
--- src/main/java/com/example/entities/Butaca.java
+++ src/main/java/com/example/entities/Butaca.java
@@ -1,29 +1,39 @@
 package com.example.entities;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Entity
-@Table(name = "TIKET")
+@Table(name = "BUTACA")
 @Data
 @Slf4j
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tiket {
+public class Butaca {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	//@Column(name = "ID")
-	private Integer tiket_id;
-	private String cliente_correo_cliente;
-	private Integer id_ocupadas;
+	//@Column(name = "id_Cine")
+	private Integer id_butaca;
 	
+	private Integer id_fila;
+	private Integer id_columna;
+
+	private Integer sala_id_sala;
+	private Integer sala_cine_id_cine;
+
+	// getters and setters
+
 	public static void info(String message) {
 		log.info(message);
 	}
